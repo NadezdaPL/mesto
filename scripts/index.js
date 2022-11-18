3fec205b1365a98bf217e7b2f113f0dda2b210bb
Nadezda <pleshkova.nadine@yandex.ru>
2022-11-18 11:21:05 -0400

    the projected was corrected

--- scripts/index.js
+++ scripts/index.js
@@ -2,29 +2,26 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const profileElement = document.querySelector('.profile');
 const profileEditButtonElement = profileElement.querySelector('.profile__edit-button');
-console.log(profileEditButtonElement);
-
-const openPopup = function (){
-  popupElement.classList.add('popup_opened');
-  console.log('Open popup clicked');
-}
-
-const closePopup = function (){
-  popupElement.classList.remove('popup_opened');
-}
-
 let formElement = document.querySelector('.popup__content');
 let nameInput = formElement.querySelector('.popup__text_content_name');
 let jobInput = formElement.querySelector('.popup__text_content_job');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
 
+const openPopup = function (){
+  popupElement.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileText.textContent;
+}
+
+const closePopup = function (){
+  popupElement.classList.remove('popup_opened');
+}
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
-
   profileTitle.textContent = nameInput.value;
   profileText.textContent = jobInput.value;
-
   closePopup();
 }
 
